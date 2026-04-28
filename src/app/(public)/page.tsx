384fc87d678a1de95a10e10c8c44befc4eab7456
Saavedra Ramon <102835876+Papyrus-Saa@users.noreply.github.com>
2026-04-28 10:34:31 +0200

    fix: correct greeting punctuation in Home component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1 className="text-2xl font-bold border dark:border-border-d border-border-l rounded">Hello, world!</h1>
+        <h1 className="text-2xl font-bold border dark:border-border-d border-border-l rounded">Hello, world!!</h1>
       </main>
     </>
   );
